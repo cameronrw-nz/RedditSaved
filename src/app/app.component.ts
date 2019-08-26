013ba45bccc58345897c77f943bf42c36f30c8d3
cameronw <cameronrw.nz@gmail.com>
2019-08-26 19:18:05 +0700

    Progress Indicator for Loading Save Posts
    Progress Indicator for Loading Save Posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,46 @@
-import { Component } from "@angular/core";
-import { RedditService } from "./reddit.service";
+import { Component } from '@angular/core';
+import { RedditService } from './reddit.service';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  isLoggingIn: boolean = false;
-  loggedInUserName: string | undefined;
-  isShowingFilter: boolean;
-  selectedSubreddits: string[];
-  numberOfSubreddits: number;
+    isLoggingIn = false;
+    loggedInUserName: string | undefined;
+    isShowingFilter: boolean;
+    selectedSubreddits: string[];
+    numberOfSubreddits: number;
+    hasFinishedLoadingSavedPosts = false;
 
-  constructor(private redditService: RedditService) {
-    this.isShowingFilter = window.innerWidth > 700;
-    this.redditService.filter.subscribe(
-      filter => (this.selectedSubreddits = filter.subreddits)
-    );
-    this.redditService.numberOfSubreddits.subscribe(numberOfSubreddits => {
-      this.numberOfSubreddits = numberOfSubreddits;
-      console.log(numberOfSubreddits);
-    });
-  }
+    constructor(private redditService: RedditService) {
+        this.isShowingFilter = window.innerWidth > 700;
+        this.redditService.filter.subscribe(
+            filter => (this.selectedSubreddits = filter.subreddits)
+        );
+        this.redditService.numberOfSubreddits.subscribe(numberOfSubreddits => {
+            this.numberOfSubreddits = numberOfSubreddits;
+        });
+        this.redditService.hasFinishedLoadingSavedPosts.subscribe(hasFinishedLoadingSavedPosts =>
+            this.hasFinishedLoadingSavedPosts = hasFinishedLoadingSavedPosts
+        );
+    }
 
-  login() {
-    this.isLoggingIn = true;
-  }
+    login() {
+        this.isLoggingIn = true;
+    }
 
-  onLoggedIn() {
-    this.isLoggingIn = false;
-    this.loggedInUserName = this.redditService.getloggedOnUserName();
-  }
+    onLoggedIn() {
+        this.isLoggingIn = false;
+        this.loggedInUserName = this.redditService.getloggedOnUserName();
+    }
 
-  toggleFilter() {
-    this.isShowingFilter = !this.isShowingFilter;
-  }
+    toggleFilter() {
+        this.isShowingFilter = !this.isShowingFilter;
+    }
 
-  clearSelectedSubreddits() {
-    this.redditService.updateFilteredSubreddits([]);
-  }
+    clearSelectedSubreddits() {
+        this.redditService.updateFilteredSubreddits([]);
+    }
 }
